3c2285ef3dfe4f1d5d7adbb143d7e2b7d1138942
PRToncloud <preritcloudtest@gmail.com>
2026-03-17 18:52:29 +0530

    Create App.jsx as the main React component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Main React Component</h1>
+            <p>Hello, world!</p>
+        </div>
+    );
+};
+
+export default App;
